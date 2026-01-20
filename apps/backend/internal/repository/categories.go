e36a4b5d052bd60c55f041bfd4e14ca4f11d8060
Mukunda Parajuli <pas079bei024@wrc.edu.np>
2026-01-20 11:02:51 +0545

    feat: implement CreateService method in CategoryRepository for adding service categories

--- apps/backend/internal/repository/categories.go
+++ apps/backend/internal/repository/categories.go
@@ -1,6 +1,13 @@
 package repository
 
-import "github.com/mukundaparajuli/fixr/internal/server"
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/mukundaparajuli/fixr/internal/model/category"
+	"github.com/mukundaparajuli/fixr/internal/server"
+)
 
 type CategoryRepository struct {
 	server *server.Server
@@ -8,4 +15,47 @@ type CategoryRepository struct {
 
 func NewCategoriesRepository(server *server.Server) *CategoryRepository {
 	return &CategoryRepository{server: server}
+}
+
+func (r *CategoryRepository) CreateService(
+	ctx context.Context,
+	UserID string,
+	payload *category.CreateCategoryPayload,
+) (*CategoryRepository, error) {
+
+	stmt := `
+		INSERT INTO 
+			service_categories(
+				user_id,
+				name,
+				description,
+				color
+			)
+		VALUES
+			(
+				@user_id,
+				@name,
+				@description,
+				@color
+			)	
+		RETURNING
+		*
+	`
+	rows, err := r.server.DB.Pool.Exec(ctx, stmt, pgx.NamedArgs{
+		"user_id": UserID,
+		"name": payload.Name,
+		"description": payload.Description,
+		"color": payload.Color
+	})
+
+	if err != nil{
+		return nil, fmt.Errorf("failed to execute create category query for user_id:%s, name:%s : %w", UserID, payload.Name, err)
+	}
+
+	categoryItem, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[category.Category])
+	if err != nil{
+		return nil, fmt.Errorf("failed to execute create category query for user_id=%s, name=%s :%w", UserID, payload.Name, err)
+	}
+
+	return &categoryItem, nil
 }
